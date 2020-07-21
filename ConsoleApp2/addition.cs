8ee77644167be6683dcaead3a8e88d2cab317335
svenschilling <35537400+svenschilling@users.noreply.github.com>
2020-07-21 14:26:28 +0200

    Projektdateien hinzufügen.

---
+++ ConsoleApp2/addition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class addition
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
